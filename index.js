c2d5fe238c9d6e968fad48cbcd27a323019c6d15
Akil Lutes <lutes.akil@gmail.com>
2021-01-15 00:56:29 -0600

    First commit Jan 15

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 import { fifaData } from './fifa.js';
 
+
 // ⚽️ M  V P ⚽️ //
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
 
+
+/* I created a ES6 sytnax .filter function that is assigned to a variable. It has conditions that needed to be met like year 2014, and if the team went to the finals. */
+const newFifa = fifaData.filter(item => item.Year === 2014 && item.Stage === 'Final')
+
 //(a) Home Team name for 2014 world cup final
-
+/* I also need to remember to include the index value when I am calling the new & updated filter array object */
+console.log(newFifa[0]['Home Team Name'])
 //(b) Away Team name for 2014 world cup final
-
+console.log(newFifa[0]['Away Team Name'])
 //(c) Home Team goals for 2014 world cup final
-
+console.log(newFifa[0]['Home Team Goals'])
 //(d) Away Team goals for 2014 world cup final
-
+console.log(newFifa[0]['Away Team Goals'])
 //(e) Winner of 2014 world cup final */
-
+console.log(newFifa[0]['Win conditions'])
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use getFinals to do the following:
@@ -24,11 +30,14 @@ Use getFinals to do the following:
 hint - you should be looking at the stage key inside of the objects
 */
 
-function getFinals(/* code here */) {
-   /* code here */
+
+function getFinals(data) {
+    /* I used the data parameter to plug in the OG fifaData object array so that I can filter when I invoke fifaData */
+   let newFinals = data.filter(data => data.Stage === 'Final')
+        return newFinals;
 }
 
-
+console.log(getFinals(fifaData));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use the higher-order function called getYears to do the following: 
@@ -36,7 +45,7 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function getFinals from task 2 
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
+function getYears() {
     /* code here */
 }
 
@@ -96,32 +105,31 @@ Create a function called `getCountryWins` that takes the parameters `data` and `
 Hint: Investigate your data to find "team initials"!
 Hint: use `.reduce` */
 
-function getCountryWins(/* code here */) {
+// function getCountryWins (/* code here */) {
 
-    /* code here */
-
-}
+//     /* code here */
+// }
 
 
 
 /* 💪💪💪💪💪 Stretch 2: 💪💪💪💪💪 
 Write a function called getGoals() that accepts a parameter `data` and returns the team with the most goals score per appearance (average goals for) in the World Cup finals */
 
-function getGoals(/* code here */) {
+// function getGoals(/* code here */) {
 
-    /* code here */
+//     /* code here */
 
-}
+// }
 
 
 /* 💪💪💪💪💪 Stretch 3: 💪💪💪💪💪
 Write a function called badDefense() that accepts a parameter `data` and calculates the team with the most goals scored against them per appearance (average goals against) in the World Cup finals */
 
-function badDefense(/* code here */) {
+// function badDefense(/* code here */) {
 
-    /* code here */
+//     /* code here */
 
-}
+// }
 
 
 /* If you still have time, use the space below to work on any stretch goals of your chosing as listed in the README file. */
